21717b287449b17a2f63b71640b9081726b70cd4
holleymcshan <holleymartinmcshan@gmail.com>
2017-10-26 09:47:01 +0200

    swaps titles and descriptions!!

--- ej02.js
+++ ej02.js
@@ -1,11 +1,20 @@
-// Al	 cargar	 la	 página,	 se	 abrirá	 modal pidiendo	email y	contraseña.	Si
-// existe	 una	 cookie	 con email especificado,	 se	 cargará	 la	 página	 y
-// se	 pondrá	 en	 la	 parte	 derecha	 de	 la	cabecera username	y	profpic.
-// Si	no	existe	la	cookie,	se	abrirá	el	ejercicio	anterior.
-
-
 // Get the modal
 var modal = document.getElementById('id01');
+
+function getCookie(cname) { //get the cookie
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -37,43 +46,46 @@ function drag(ev) {
 function drop(ev){
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
-  console.log("drop",data);
+  console.log("drop",data); //the one we need to swap with
   var destination = ev.target;
   var origen = document.getElementById(data);
   var copyDestination=destination.src;
 
+  //swap 1
   console.log($('#'+ev.target.id).children().first()[0].src);
   var mainVidInf = $('#'+ev.target.id).children().first()[0].src
   console.log("mainvidinf: "+mainVidInf)
+
+  //swap 2
   $('#'+ev.target.id).children().first()[0].src= $('#'+data).children().first()[0].src;
   $('#'+ev.target.id).get(0).load();
 
 
-  console.log("findwhichvid" +ev.target.id);
-  setInfo(ev.target.id);
-
-
-
-
-
-
-  $('#'+data).children().first()[0].src = mainVidInf;
-  console.log('secondaryvid'+$('#'+data).children().first()[0].src)
   $('#'+data).get(0).load();
-
-
+  console.log($('#'+data).get(0).id);
+  setInfo($('#'+data).get(0).id); //swap information of mainvid and dragged vid
 }
+
 
 function setInfo(id){
-  var index = id.substring(id.indexOf('o')+ 1) - 1;
-  console.log(index);
-  console.log(document.getElementById(id))            ////////////////////////////////////////
-  console.log('nextsev'+document.getElementById(id).innerHTML)
-  document.getElementById(id).innerHTML = vidinfo[index].title;
-  document.getElementById("description0" + (index+1)).innerHTML= vidinfo[index].description;   /////
+  var index = id.substring(id.indexOf('o')+ 1); //get vid number
+
+  var oldTitle = document.getElementById("title01").innerHTML;  //temp for swap
+  var oldDesc = document.getElementById("description01").innerHTML;  //temp for swap
+  console.log('old des for main'+ oldDesc);
+
+  //swap 1
+  document.getElementById("title01").innerHTML = document.getElementById("title0"+index).innerHTML;
+  document.getElementById("description01").innerHTML = document.getElementById("description0"+index).innerHTML
+
+  //swap 2
+  document.getElementById("title0"+index).innerHTML = oldTitle;
+  document.getElementById("description0"+index).innerHTML = oldDesc;
+  console.log('Should be Blytheblair ....'+document.getElementById('title01').innerHTML);
 }
 
-function expand_collapse(id) {
+
+function expand_collapse(id) {  //expand / collapse description
    var e = document.getElementById(id);
    if(e.style.display = 'none'){
       e.style.display = 'block';
@@ -84,26 +96,8 @@ function expand_collapse(id) {
    console.log("expand called")
    console.log(e)
 }
-var ivideo1 = {
-  title: "OCD - Neil Hilbord",
-  description: "This video is one of Button Poetry's most popular posts. The spoken word poem was a contest winner at Rustbelt 2013",
-};
 
-var ivideo2 = {
-  title: "Friend Zone - Dylan Garity",
-  description: "Performing at the Saint Paul Soapboxing Poetry Slam in May 2013."
-};
-var ivideo3 = {
-  title: "Girl Code 101 - Blythe Baird",
-  description: "Blythe Baird, performing for Chicago at the 2014 National Poetry Slam."
-};
-var ivideo4 = {
-  title: "Shrinking Women - Lily Meyers",
-  description: "Lily Myers, performing for Wesleyan University at the 2013 College Unions Poetry Slam Invitational. This poem was awarded Best Love Poem at the tournament."
-};
 
-var vidinfo = [ivideo1, ivideo2, ivideo3, ivideo4];
-console.log(vidinfo);
 function reply_click()
 {
   console.log(event.srcElement.id);
